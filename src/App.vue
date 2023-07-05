307b53b0b9cecac3cb9cc0490de711203c0e10a8
Yegor Zakharov <yegor.zakharov99@gmail.com>
2023-07-05 21:31:21 +0200

    delete Greet component

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,6 @@
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
 import { onMounted } from "vue";
-import Greet from "./components/Greet.vue";
 import MatchLoadouts from "./pages/MatchLoadouts.vue";
 import { Command } from "@tauri-apps/api/shell";
 
@@ -22,12 +21,4 @@ onMounted(async () => {
   </div>
 </template>
 
-<style scoped>
-.logo.vite:hover {
-  filter: drop-shadow(0 0 2em #747bff);
-}
-
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #249b73);
-}
-</style>
+<style scoped></style>
