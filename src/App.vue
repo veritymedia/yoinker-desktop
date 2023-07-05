ec5a357e76de8aed98252f802a88a457180364ee
Yegor Zakharov <yegor.zakharov99@gmail.com>
2023-07-05 20:57:24 +0200

    copied some components

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,37 @@
-<script setup lang="ts">
+<script setup>
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
 import { onMounted } from "vue";
 import Greet from "./components/Greet.vue";
 import { Command } from "@tauri-apps/api/shell";
 
-onMounted(() => {
-  {
-    const command = Command.sidecar("");
-  }
+// import execa from "execa";
+// import fs from "fs";
+
+// let extension = "";
+// if (process.platform === "win32") {
+//   extension = ".exe";
+// }
+
+// async function main() {
+//   const rustInfo = (await execa("rustc", ["-vV"])).stdout;
+//   const targetTriple = /host: (\S+)/g.exec(rustInfo)[1];
+//   if (!targetTriple) {
+//     console.error("Failed to determine platform target triple");
+//   }
+//   fs.renameSync(
+//     `src-tauri/binaries/sidecar${extension}`,
+//     `src-tauri/binaries/sidecar-${targetTriple}${extension}`
+//   );
+// }
+
+onMounted(async () => {
+  // main().catch((e) => {
+  //   throw e;
+  // });
+
+  const command = Command.sidecar("binaries/yoinker-backend");
+  const output = await command.execute();
 });
 </script>
 
