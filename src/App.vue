ed0f75d68378676c215a73ca442e9c0504901f5a
Yegor Zakharov <yegor.zakharov99@gmail.com>
2023-07-05 21:28:37 +0200

    added TW

--- src/App.vue
+++ src/App.vue
@@ -3,72 +3,22 @@
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
 import { onMounted } from "vue";
 import Greet from "./components/Greet.vue";
+import MatchLoadouts from "./pages/MatchLoadouts.vue";
 import { Command } from "@tauri-apps/api/shell";
 
-// import execa from "execa";
-// import fs from "fs";
-
-// let extension = "";
-// if (process.platform === "win32") {
-//   extension = ".exe";
-// }
-
-// async function main() {
-//   const rustInfo = (await execa("rustc", ["-vV"])).stdout;
-//   const targetTriple = /host: (\S+)/g.exec(rustInfo)[1];
-//   if (!targetTriple) {
-//     console.error("Failed to determine platform target triple");
-//   }
-//   fs.renameSync(
-//     `src-tauri/binaries/sidecar${extension}`,
-//     `src-tauri/binaries/sidecar-${targetTriple}${extension}`
-//   );
-// }
-
 onMounted(async () => {
-  // main().catch((e) => {
-  //   throw e;
-  // });
-
   const command = Command.sidecar("binaries/yoinker-backend");
-  const output = await command.execute();
+  try {
+    const output = await command.execute();
+  } catch (err) {
+    console.log("yoinker backend err: ", err);
+  }
 });
 </script>
 
 <template>
-  <div class="container">
-    <h1>Welcome to Tauri!</h1>
-
-    <div class="row">
-      <a href="https://vitejs.dev" target="_blank">
-        <img src="/vite.svg" class="logo vite" alt="Vite logo" />
-      </a>
-      <a href="https://tauri.app" target="_blank">
-        <img src="/tauri.svg" class="logo tauri" alt="Tauri logo" />
-      </a>
-      <a href="https://vuejs.org/" target="_blank">
-        <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-      </a>
-    </div>
-
-    <p>Click on the Tauri, Vite, and Vue logos to learn more.</p>
-
-    <p>
-      Recommended IDE setup:
-      <a href="https://code.visualstudio.com/" target="_blank">VS Code</a>
-      +
-      <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
-      +
-      <a href="https://github.com/tauri-apps/tauri-vscode" target="_blank"
-        >Tauri</a
-      >
-      +
-      <a href="https://github.com/rust-lang/rust-analyzer" target="_blank"
-        >rust-analyzer</a
-      >
-    </p>
-
-    <Greet />
+  <div class="container bg-blue-500">
+    <MatchLoadouts />
   </div>
 </template>
 
